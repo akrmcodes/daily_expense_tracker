3710bc75bec2e0d0580a2a3b157a1ffcfe905495
Akrm Qubati <akrm.codes@gmail.com>
2025-08-10 23:01:09 +0300

    delete init package form account_details_screen.dart

--- lib/views/account_details_screen.dart
+++ lib/views/account_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-import 'package:intl/intl.dart';
 
 import '../providers/app_state_provider.dart';
 import '../widgets/transaction_card.dart';
